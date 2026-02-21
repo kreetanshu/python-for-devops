e920d615f87d5a0539c2b575472c1634815ef10b
Reetanshu Kumar <kreetanshu@gmail.com>
2026-02-21 19:32:45 +0530

    Create playwright/playwright.config.ts

---
+++ playwright/playwright.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  use: {
+    baseURL: 'https://www.epam.com',
+    trace: 'on-first-retry',
+  },
+  reporter: [['list']],
+});
